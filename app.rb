4c511109bae2c6c92bdf0cba5b65322d6f21084a
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-21 11:47:17 -0600

    commit tree 0d2deefcfb90cad7336da579e92bf0801df76465

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ade"
+  end
+
+  get '/hometown' do
+    "My hometown is San Jose"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Somebody to Love"
+  end
 end
